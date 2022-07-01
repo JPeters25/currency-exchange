77d318d43ab3f2063437d5725b398f6872cfafb5
Justin Peters <JMarvel@gmail.com>
2022-07-01 09:00:49 -0700

    finish setup for project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import Exchange from './exchange.js';
+
 $(document).ready(function() {
   $('#triangle-checker-form').submit(function(event) {
     event.preventDefault();
