bc5e545277f69883bd6c14f9f2cb8f66084bdced
Justin Peters <JMarvel@gmail.com>
2022-07-01 10:09:52 -0700

    add API link and basic API call functionality

--- src/index.js
+++ src/index.js
@@ -2,10 +2,30 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Exchange from './exchange.js';
 
 $(document).ready(function() {
-  $('#triangle-checker-form').submit(function(event) {
-    event.preventDefault();
-  })
-})
+  $('#convert-currency').click(function() {
+    const amount = $('#dollar-amount').val();
+    $('#dollar-amount').val("");
+
+    let request = new XMLHttpRequest();
+    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+
+    request.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        /* eslint-disable */
+        getElements(response);
+        /* eslint-enable*/
+      }
+    };
+    request.open("GET", url, true);
+    request.send();
+    /* eslint-disable */
+    function getElements(response) {
+      $('.showUSD').text(`USD = $ ${amount} is ${response.conversion_rates.USD}`);
+    }
+    /* eslint-enable*/
+  });
+});
+
